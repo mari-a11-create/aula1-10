bfa7955f4f651cb70cc3c129bf4e4e41295e8063
MARISOL PUNTUSIL ORMACHEA <marisol.pormachea@senacsp.edu.br>
2024-10-01 17:25:54 -0300

    first commit

---
+++ exercicio-1/script.js
@@ -0,0 +1,11 @@
+/* ExercicioN 1: contagem simples
+**objetivo** Crie um programa que conte de 1 a 5 e imprima cada
+
+** Dica** Use umloop `while e uma variável de controle.*/
+
+
+let contador = 1;
+while(contador <=5) {
+    console.log(contador)
+        contador +=1;
+}
---
+++ exercicio-5/script.js
@@ -0,0 +1,11 @@
+let contador =1
+const numero =5
+
+while(contador <=10
+    // console.log(contador +"x"+ numero+"=" +(numero contador));
+    console.log( ${numero} x ${contador} = ${numero * contador}` )
+    contador +=2:
+}
+for (let contador =1;contador<=10;contador++)
+    console.log)(`${numero} x ${contador} = ${numero* contador})
+}
